358e5e7967ef768eb4e0818f6b4a1d41f425f86c
Richard Wen <rwen1@umd.edu>
2025-01-14 13:00:43 -0500

    Implemented centrality test with random starting points

--- analysis/centrality.cpp
+++ analysis/centrality.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <atomic>
 #include <fstream>
+#include <vector>
+#include <utility>
+#include <atomic>
+
+#include <parlay/sequence.h>
+#include <parlay/parallel.h>
+#include <parlay/primitives.h>
+#include <parlay/random.h>
 
 #include "custom_beam_search.h"
 
-using index_type = uint32_t;
-using value_type = float;
-using point_type = parlayANN::Euclidian_Point<value_type>;
-using point_range_type = parlayANN::PointRange<point_type>;
+using index_t = uint32_t;
+using value_t = float;
+using point_t = parlayANN::Euclidian_Point<value_t>;
+using point_range_t = parlayANN::PointRange<point_t>;
 
 // Load a graph, then run multiple custom_beam_searches on it with random starting points,
 // counting the number of times each edge is visited, which we'll call its centrality.
@@ -16,13 +22,18 @@ using point_range_type = parlayANN::PointRange<point_type>;
 int main(int argc, char* argv[]) {
     char g_file[] = "/ssd1/anndata/ANNbench_/data/sift/graphs/sift_vamana_R32_L64_alpha1.2_k10_nthreads64.graph";
     char p_file[] = "/ssd1/anndata/ANNbench_/data/sift/sift_base.fbin";
+    char o_file[] = "centrality.bin";
+    const size_t num_queries = 10000;
 
-    parlayANN::Graph<index_type> graph(g_file);
-    point_range_type points(p_file);
+    std::cout << "Loading graph from " << g_file << std::endl;
+    parlayANN::Graph<index_t> graph(g_file);
+    std::cout << "Loading base points from " << p_file << std::endl;
+    point_range_t points(p_file);
     if (graph.size() != points.size()) {
-        std::cerr << "Graph and point range sizes do not match" << std::endl;
+        std::cerr << "Graph and point range sizes do not match. Aborting..." << std::endl;
         return 1;
     }
+    const size_t max_edges = graph.size() * graph.max_degree();
 
     auto BP = parlayANN::QueryParams(
         10, // k
@@ -32,43 +43,71 @@ int main(int argc, char* argv[]) {
         graph.max_degree() // degree limit
     );
 
-    srand(time(NULL));
-    auto centrality = parlay::tabulate<std::atomic<uint32_t>>(graph.size() * graph.max_degree(), [] (size_t i) {return 0;});
-    uint32_t start_point = rand() % points.size(), query_point = rand() % points.size();
-    parlay::sequence<index_type> starting_points(1, start_point);
-    custom_beam_search(
-        graph, points[query_point], points, points[query_point], points,
-        starting_points, BP,
-        [] (index_type i) {},
-        [&] (index_type i, index_type j) {
-            // Find which of the max_degree slots in the edge array this edge is in
-            uint32_t slot = -1;
-            for (uint32_t k = 0; k < graph.max_degree(); k++) {
-                if (graph[i][k] == j) {
-                    slot = k;
-                    break;
-                }
-            }
-            if (slot != -1) centrality[i * graph.max_degree() + slot]++;
+    std::cout << "Generating random queries..." << std::flush;
+    parlay::random_generator gen;
+    std::uniform_int_distribution<index_t> dis(0, points.size() - 1);
+    auto queries = parlay::tabulate<std::pair<index_t, index_t>>(num_queries,
+        [&] (size_t i) {
+            auto r = gen[i];
+            return std::make_pair(dis(r), dis(r));
         }
     );
+    std::cout << "Done" << std::endl;
 
-    // Compute edge lengths
-    auto edge_lengths = parlay::tabulate<value_type>(graph.size() * graph.max_degree(),
-        [&] (size_t i) -> value_type {
+    // Run beam search with random start and query points, tracking centrality
+    std::cout << "Running beam search..." << std::flush;
+    auto centrality = parlay::tabulate<std::atomic<uint32_t>>(max_edges, [] (size_t i) {return 0;});
+    parlay::parallel_for(0, num_queries, [&] (size_t i) {
+        index_t start_point = queries[i].first, query_point = queries[i].second;
+        parlay::sequence<index_t> starting_points(1, start_point);
+        custom_beam_search(
+            graph, points[query_point], points, points[query_point], points,
+            starting_points, BP,
+            [] (index_t i) {},
+            [&] (index_t i, index_t j) {
+                // Find the index of this edge in the adjacency list
+                size_t slot = -1;
+                for (size_t k = 0; k < graph.max_degree(); k++) {
+                    if (graph[i][k] == j) {
+                        slot = k;
+                        break;
+                    }
+                }
+                if (slot == -1) return;
+                if (i > j) std::swap(i, j);
+                centrality[i * graph.max_degree() + slot]++;
+            }
+        );
+    });
+    std::cout << "Done" << std::endl;
+
+    // Compute edge lengths for valid edges
+    std::cout << "Processing results..." << std::flush;
+    auto edge_lengths = parlay::tabulate<value_t>(max_edges,
+        [&] (size_t i) -> value_t {
             if (i % graph.max_degree() >= graph[i / graph.max_degree()].size()) return -1;
+            else if (i / graph.max_degree() > graph[i / graph.max_degree()][i % graph.max_degree()]) return -1;
             else return points[graph[i / graph.max_degree()][i % graph.max_degree()]].distance(points[i / graph.max_degree()]);
         }
     );
 
-    // Output edge lengths and centralities
-    char out_file[] = "centrality.txt";
-    std::ofstream out(out_file);
-    for (size_t i = 0; i < edge_lengths.size(); i++) {
-        if (edge_lengths[i] == -1) continue;
-        out << edge_lengths[i] << " " << centrality[i] << std::endl;
-    }
-    out.close();
+    // Filter out invalid and duplicate edges
+    auto zipped = parlay::delayed_tabulate<std::pair<value_t, uint32_t>>(max_edges,
+        [&] (size_t i) -> std::pair<value_t, uint32_t> {
+            return std::make_pair(edge_lengths[i], centrality[i].load());
+        }
+    );
+    auto filtered = parlay::filter(zipped,
+        [&] (std::pair<value_t, uint32_t> a) {
+            return a.first != -1;
+        }
+    );
+    std::cout << "Done" << std::endl;
+
+    // Output edge lengths and centralities to file
+    auto chars = parlay::to_chars(filtered);
+    parlay::chars_to_file(chars, o_file);
+    std::cout << "Results written to " << o_file << std::endl;
  
     return 0;
 }

cb2d99e572efb294fe9e9a4f01599ef124235052
Richard Wen <rwen1@umd.edu>
2025-02-07 13:55:21 -0500

    Constructing graph by pruning ground truth neighborhood

--- analysis/prune_neighborhood.cpp
+++ analysis/prune_neighborhood.cpp
@@ -17,16 +17,19 @@
 #include "vamana/neighbors.h"
 
 #define DATASET 1
+#define EXACT true
 
 int main(int argc, char *argv[]) {
     #if DATASET == 1
         std::string base_path = "/ssd1/anndata/ANNbench_/data/sift/sift_base.fbin";
         std::string query_path = "/ssd1/anndata/ANNbench_/data/sift/sift_query.fbin";
         std::string gt_path = "/ssd1/anndata/ANNbench_/data/sift/sift_query.gt";
+        std::string exact_path = "/ssd1/richard/anndata/sift_1000nn.gt";
     #elif DATASET == 2
         std::string base_path = "/ssd1/anndata/ANNbench_/data/text2image1M/base.fbin";
         std::string query_path = "/ssd1/anndata/ANNbench_/data/text2image1M/query10K.fbin";
         std::string gt_path = "/ssd1/anndata/ANNbench_/data/text2image1M/text2image1M.gt";
+        std::string exact_path = "/ssd1/richard/anndata/t2i_1000nn.gt";
     #endif
 
     // Build an index out of the base points
@@ -40,20 +43,37 @@ int main(int argc, char *argv[]) {
     using PointRangeType = parlayANN::PointRange<PointType>;
     using GraphType = parlayANN::Graph<index_t>;
     size_t max_degree = 32;
-    auto B = PointRangeType(base_path.data());
-    auto Q = PointRangeType(query_path.data());
-    auto GT = parlayANN::groundTruth<index_t>(gt_path.data());
-    auto G = GraphType(max_degree, B.size());
-    auto BP = parlayANN::BuildParams(max_degree, 64, 1.2, 2, false);
-    auto I = parlayANN::knn_index<PointRangeType, PointRangeType, index_t>(BP);
-    parlayANN::stats<index_t> sbuild(size_t(B.size()));
-    I.build_index(G, B, B, sbuild);
+    #if EXACT
+        auto B = PointRangeType(base_path.data());
+        auto Q = PointRangeType(query_path.data());
+        auto GT = parlayANN::groundTruth<index_t>(exact_path.data());
+        auto neighborhood = parlayANN::groundTruth<index_t>(exact_path.data());
+        auto G = GraphType(max_degree, B.size());
+        auto BP = parlayANN::BuildParams(max_degree, 64, 1.2, 2, false);
+        auto I = parlayANN::knn_index<PointRangeType, PointRangeType, index_t>(BP);
 
-    // Use the index to obtain the approximate neighborhood of each point
-    std::cout << "Obtaining approximate neighborhoods..." << std::endl;
-    auto QP = parlayANN::QueryParams(500, 1000, 1.35, B.size(), max_degree, 1);
-    auto neighbors = parlayANN::qsearchAll<PointRangeType, PointRangeType, PointRangeType, index_t>(B, B, B, G, B, B, B, sbuild, 0, QP);
+        auto neighbors = parlay::tabulate(B.size(), [&](size_t i) {
+            return parlay::tabulate(neighborhood.dim, [&](size_t j) {
+                return neighborhood.coordinates(i, j);
+            });
+        });
+    #else
+        auto B = PointRangeType(base_path.data());
+        auto Q = PointRangeType(query_path.data());
+        auto GT = parlayANN::groundTruth<index_t>(gt_path.data());
+        auto G = GraphType(max_degree, B.size());
+        auto BP = parlayANN::BuildParams(max_degree, 64, 1.2, 2, false);
+        auto I = parlayANN::knn_index<PointRangeType, PointRangeType, index_t>(BP);
+        parlayANN::stats<index_t> sbuild(size_t(B.size()));
+        I.build_index(G, B, B, sbuild);
 
+        // Use the index to obtain the approximate neighborhood of each point
+        std::cout << "Obtaining approximate neighborhoods..." << std::endl;
+        auto QP = parlayANN::QueryParams(500, 1000, 1.35, B.size(), max_degree, 1);
+        auto neighbors = parlayANN::qsearchAll<PointRangeType, PointRangeType, PointRangeType, index_t>(B, B, B, G, B, B, B, sbuild, 0, QP);
+    #endif
+
+    // Prune the neighborhoods
     std::cout << "Pruning neighborhoods..." << std::endl;
     auto new_G = GraphType(max_degree, B.size());
     parlay::parallel_for(0, B.size(), [&](size_t i) {
